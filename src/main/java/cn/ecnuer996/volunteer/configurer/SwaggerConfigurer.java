ce26d6dec568d6578879635eeb69b9b5d161e5aa
okarinx <xs74678@hotmail.com>
2020-02-02 17:40:25 +0800

    fix swagger

--- src/main/java/cn/ecnuer996/volunteer/configurer/SwaggerConfigurer.java
+++ src/main/java/cn/ecnuer996/volunteer/configurer/SwaggerConfigurer.java
@@ -2,6 +2,9 @@ package cn.ecnuer996.volunteer.configurer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,10 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfigurer {
+public class SwaggerConfigurer extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 // 部署时为了保证swagger可用需要设置host
                 // 在本地查看Swagger需要将下一行注释
@@ -25,5 +28,29 @@ public class SwaggerConfigurer {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    /**
+     * 配置swagger访问问题
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs?group=restful-api");
+        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+    }
+
+    /**
+     * 配置swagger访问问题
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
